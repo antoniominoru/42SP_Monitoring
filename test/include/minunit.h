5b7ede89e7af51aacc49f37bda58f39387433086
aminoru- <aminoru-@student.42sp.org.br>
2022-08-01 01:51:20 +0200

    test refactor

--- test/include/minunit.h
+++ test/include/minunit.h
@@ -6,11 +6,16 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 15:18:23 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/07/31 16:32:08 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/07/31 18:12:54 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINUNIT_H
+# define MINUNIT_H
+
 /* file: minunit.h */
- #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
- #define mu_run_test(test) do { char *message = test(); g_tests_run++; if (message) return message; } while (0)
- extern int g_tests_run;
+# define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+# define mu_run_test(test) do { char *message = test(); g_tests_run++; if (message) return message; } while (0)
+extern int g_tests_run;
+
+ #endif
--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 14:53:44 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/07/31 17:50:27 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/07/31 18:20:47 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	g_tests_run = 0;
 
-static char	*test_db_valid(void)
+static char	*test_db_valid1(void)
 {
 	t_list	*list;
 	int		ok;
@@ -24,15 +24,71 @@ static char	*test_db_valid(void)
 	mu_assert("ERROR: db_valid()", db_valid(list) == ok);
 	list = ft_lstnew("intra	HTTP	intra.42.fr	GET	200");
 	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	list = ft_lstnew("intra	HTTP	intra.42.fr	GET	200	120	tt");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	free(list);
+	return (0);
+}
+
+static char	*test_db_valid2(void)
+{
+	t_list	*list;
+	int		ok;
+
+	ok = 0;
 	list = ft_lstnew("game ping test	PING	game.42sp.org.br	60");
 	mu_assert("ERROR: db_valid()", db_valid(list) == ok);
-	list = ft_lstnew("game ping test	PING	game.42sp.org.br	60	rr");
+	list = ft_lstnew("game ping test	PING	game.42sp.org.br");
 	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
-	list = ft_lstnew("game ping test	DNS	game.42sp.org.br	60	tt");
+	list = ft_lstnew("game ping test	PING	game.42sp.org.br	60	tt");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	free(list);
+	return (0);
+}
+
+static char	*test_db_valid3(void)
+{
+	t_list	*list;
+	int		ok;
+
+	ok = 0;
+	list = ft_lstnew("game dns test	DNS	game.42sp.org.br	60	8.8.8.8");
 	mu_assert("ERROR: db_valid()", db_valid(list) == ok);
-	list = ft_lstnew("game ping test	DNS	game.42sp.org.br	60	tt	rr");
+	list = ft_lstnew("game dns test	DNS	game.42sp.org.br	60");
 	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
-	list = ft_lstnew("game ping test	PIN	game.42sp.org.br	60	tt");
+	list = ft_lstnew("game dns test	DNS	game.42sp.org.br	60	8.8.8.8	tt");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	free(list);
+	return (0);
+}
+
+static char	*test_db_valid4(void)
+{
+	t_list	*list;
+	int		ok;
+
+	ok = 0;
+	list = ft_lstnew("game dns test	DrS	game.42sp.org.br	60	8.8.8.8");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	list = ft_lstnew("game dns test	DrS	game.42sp.org.br	60");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	list = ft_lstnew("game dns test	DrS	game.42sp.org.br	60	8.8.8.8	tt");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	free(list);
+	return (0);
+}
+
+static char	*test_db_valid5(void)
+{
+	t_list	*list;
+	int		ok;
+
+	ok = 0;
+	list = ft_lstnew("game dns test	DN	game.42sp.org.br	60	8.8.8.8");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	list = ft_lstnew("game dns test	DN	game.42sp.org.br	60");
+	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
+	list = ft_lstnew("game dns test	DN	game.42sp.org.br	60	8.8.8.8	tt");
 	mu_assert("ERROR: db_valid()", db_valid(list) != ok);
 	free(list);
 	return (0);
@@ -40,7 +96,11 @@ static char	*test_db_valid(void)
 
 static char	*all_tests(void)
 {
-	mu_run_test(test_db_valid);
+	mu_run_test(test_db_valid1);
+	mu_run_test(test_db_valid2);
+	mu_run_test(test_db_valid3);
+	mu_run_test(test_db_valid4);
+	mu_run_test(test_db_valid5);
 	return (0);
 }
 
