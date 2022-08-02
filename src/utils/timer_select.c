8806018a3a4401388c707bb2638415f8d2924353
aminoru- <aminoru-@student.42sp.org.br>
2022-08-02 21:33:31 +0200

    test

--- src/utils/timer_select.c
+++ src/utils/timer_select.c
@@ -6,7 +6,7 @@
 /*   By: aminoru- <aminoru-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 16:25:44 by aminoru-          #+#    #+#             */
-/*   Updated: 2022/08/02 21:08:50 by aminoru-         ###   ########.fr       */
+/*   Updated: 2022/08/02 21:28:00 by aminoru-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,20 @@ void	timer_selected(t_list *list, int timer)
 		if (!ft_strncmp(teste[1], "HTTP", ft_strlen(teste[1])))
 			if (timer % ft_atoi(teste[n_words - 1]) == 0)
 			{
-				system("echo ---------------- >> log/monitoring.log");
 				system("curl --head -s intra.42.fr 2>&1 | sed '$d' | sed \"1s/^/Monitoramento: HTTP /\" | tee -a log/monitoring.log | grep Monitoramento:");
+				system("echo ---------------- >> log/monitoring.log");
 			}
 		if (!ft_strncmp(teste[1], "PING", ft_strlen(teste[1])))
 			if (timer % ft_atoi(teste[n_words - 1]) == 0)
 			{
-				system("echo ---------------- >> log/monitoring.log");
 				system("ping -c1 intra.42.fr 2>&1 | sed '/^$/d'| sed \"2s/^/Monitoramento: PING /\" | tee -a log/monitoring.log | grep Monitoramento:");
+				system("echo ---------------- >> log/monitoring.log");
 			}
 		if (!ft_strncmp(teste[1], "DNS", ft_strlen(teste[1])))
 			if (timer % ft_atoi(teste[n_words - 2]) == 0)
 			{
+				system("nslookup intra.42.fr 8.8.8.8 2>&1 | sed '/^$/d'| sed \"1s/^/Monitoramento: DNS /\" | tee -a log/monitoring.log | grep Monitoramento:");
 				system("echo ---------------- >> log/monitoring.log");
-				system("nslookup intra.42.fr 8.8.8.8 2>&1 | sed '/^$/d'| sed \"3s/^/Monitoramento: DNS /\" | tee -a log/monitoring.log | grep Monitoramento:");
 			}
 		temp = temp->next;
 	}
